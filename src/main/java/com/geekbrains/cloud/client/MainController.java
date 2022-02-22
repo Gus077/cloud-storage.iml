450ba2b058d4629b074fae7596b22ae812f8bc16
Gus077 <firo077@yandex.ru>
2022-02-22 16:42:59 +0400

    1

---
+++ src/main/java/com/geekbrains/cloud/client/MainController.java
@@ -0,0 +1,145 @@
+package com.geekbrains.cloud.client;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class MainController implements Initializable {
+
+    private static final int BUFFER_SIZE = 8192;
+
+    public TextField clientPath;
+    public TextField serverPath;
+    public ListView<String> clientView;
+    public ListView<String> serverView;
+    private File currentDirectory;
+    private File serverDirectory;
+
+    private DataInputStream is;
+    private DataOutputStream os;
+    private byte[] buf;
+
+    // Platform.runLater(() -> {})
+    private void updateClientView() {
+        Platform.runLater(() -> {
+            clientPath.setText(currentDirectory.getAbsolutePath());
+            clientView.getItems().clear();
+            clientView.getItems().add("...");
+            clientView.getItems()
+                    .addAll(currentDirectory.list());
+        });
+    }
+
+    private void updateServerView() {
+        Platform.runLater(() -> {
+            serverPath.setText(serverDirectory.getAbsolutePath());
+            serverView.getItems().clear();
+            serverView.getItems().add("...");
+            serverView.getItems().addAll(serverDirectory.list());
+        });
+
+    }
+    // download file on server
+    public void download(ActionEvent actionEvent) throws IOException {
+        String item = serverView.getSelectionModel().getSelectedItem();
+        File selected = serverDirectory.toPath().resolve(item).toFile();
+        if (selected.isFile()){
+            os.writeUTF("#file_download#");
+            os.writeUTF(selected.getName());
+            os.writeLong(selected.length());
+            try(InputStream fis = new FileInputStream(selected)){
+                while (fis.available() > 0) {
+                    int readBytes = fis.read(buf);
+                    os.write(buf, 0, readBytes);
+                }
+            }
+            os.flush();
+        }
+    }
+
+    // upload file to server
+    public void upload(ActionEvent actionEvent) throws IOException {
+        String item = clientView.getSelectionModel().getSelectedItem();
+        File selected = currentDirectory.toPath().resolve(item).toFile();
+        if (selected.isFile()) {
+            os.writeUTF("#file_message#");
+            os.writeUTF(selected.getName());
+            os.writeLong(selected.length());
+            try (InputStream fis = new FileInputStream(selected)) {
+                while (fis.available() > 0) {
+                    int readBytes = fis.read(buf);
+                    os.write(buf, 0, readBytes);
+                }
+            }
+            os.flush();
+        }
+    }
+
+    private void initNetwork() {
+        try {
+            buf = new byte[BUFFER_SIZE];
+            Socket socket = new Socket("localhost", 8189);
+            is = new DataInputStream(socket.getInputStream());
+            os = new DataOutputStream(socket.getOutputStream());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        currentDirectory = new File(System.getProperty("user.home"));
+
+        // не понимаю как можно указать путь иначе, если пишу просто "server", то не получается подняться на папку выше.
+        serverDirectory = new File("D:\\Java dropbox\\cloud-storage-feb-3-master\\server");
+
+
+        // run in FX Thread
+        // :: - method reference
+        updateServerView();
+        updateClientView();
+        initNetwork();
+        clientView.setOnMouseClicked(e -> {
+            if (e.getClickCount() == 2) {
+                String item = clientView.getSelectionModel().getSelectedItem();
+                if (item.equals("...")) {
+                    currentDirectory = currentDirectory.getParentFile();
+                    updateClientView();
+                } else {
+                    File selected = currentDirectory.toPath().resolve(item).toFile();
+                    if (selected.isDirectory()) {
+                        currentDirectory = selected;
+                        updateClientView();
+                    }
+                }
+            }
+        });
+        serverView.setOnMouseClicked(e -> {
+            if (e.getClickCount() == 2){
+                String item = serverView.getSelectionModel().getSelectedItem();
+                if (item.equals("...")) {
+                    serverDirectory = serverDirectory.getParentFile();
+                    updateServerView();
+                } else {
+                    File selected = serverDirectory.toPath().resolve(item).toFile();
+                    if (selected.isDirectory()) {
+                        serverDirectory = selected;
+                        updateServerView();
+                    }
+                }
+            }
+        });
+    }
+}
